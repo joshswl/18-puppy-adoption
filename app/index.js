c43abba1bdcdc5e730675df3777e6877ead4adc9
Josh Lee <joshswl@gmail.com>
2017-03-14 16:19:51 -0500

    setup index.js file

--- app/index.js
+++ app/index.js
@@ -4,3 +4,21 @@ import VueRouter from 'vue-router';
 
 import App from './routes/application.vue';
 import Index from './routes/index.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+const app = new Vue({ ...App, router });
+app.$mount('.app');
