78ac1e2b2abf3ad99653408a8d8516cc845312db
Josh Lee <joshswl@gmail.com>
2017-03-21 16:59:21 -0500

    styled nav

--- app/reducer/pups.js
+++ app/reducer/pups.js
@@ -3,11 +3,11 @@ import Lodash from 'lodash';
 export default function pups(state = [], action) {
   switch (action.type) {
     case 'PUP@FINDALL_COMPLETE':
-      return _.unionBy(action.data, state, '_id');
+      return Lodash.unionBy(action.data, state, '_id');
     case 'PUP@FINDONE_COMPLETE':
     case 'PUP@CREATE_COMPLETE':
-    case 'PUP@UPDATE_COMPLETE';
-      return _.unionBy([action.data], state, '_id');
+    case 'PUP@UPDATE_COMPLETE':
+      return Lodash.unionBy([action.data], state, '_id');
     default:
       state;
   }
--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,6 +1,18 @@
 <template lang="html">
-  <div class="">
-
+  <div class="app">
+    <nav class="nav">
+      <div class="nav__left">
+        <a href="" class="nav-item is-brand">
+          <h2 class="nav-item is-brand">
+            <a href="/" class="nav-item is-active">Puppies</a>
+          </h2>
+        </a>
+      </div>
+      <div class="nav-right nav-menu">
+        <a href="/" class="nav-item">All Puppies</a>
+        <a href="/new" class="nav-item">Add Puppy</a>
+      </div>
+    </nav>
   </div>
 </template>
 
